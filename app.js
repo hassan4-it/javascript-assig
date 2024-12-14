76a5cc69d9f70a7862422104815e01d78fb22b2f
hassan4-it <sshorlets@gmail.com>
2024-12-14 09:47:13 +0500

    asigment chapter 1 to 5

---
+++ app.js
@@ -0,0 +1,25 @@
+// console.log("hello world");
+
+// var username = "hassan";
+// console.log(username)
+
+// var massage ="hello world";
+// alert (massage);
+
+
+// var username = "hassan";
+// alert (username);
+
+// var age = "25 years old";
+// alert (age);
+
+// var data = "certified mobile app devlopment";
+// alert (data);
+
+
+// var email = "Mhassaan476@gmail.com";
+// alert (email);
+ 
+// var book ="A smarter way to learn javascript,";
+// alert (book);
+
